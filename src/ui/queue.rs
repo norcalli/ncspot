e69e20e580bc57be67863bc1b8ea137fdf6ba709
Ashkan Kiani <ashkan@zephyr>
2019-03-03 20:43:51 -0800

    Use selectview instead of listview and get max improvement.

--- src/ui/queue.rs
+++ src/ui/queue.rs
@@ -2,6 +2,7 @@ use cursive::direction::Direction;
 use cursive::event::Key;
 use cursive::traits::Boxable;
 use cursive::traits::Identifiable;
+use cursive::traits::Scrollable;
 use cursive::views::*;
 use cursive::Cursive;
 
@@ -18,20 +19,31 @@ use crate::spotify::{PlayerState, Spotify};
 use crate::ui::trackbutton::TrackButton;
 
 pub struct QueueView {
-    pub view: Option<Panel<LinearLayout>>,
-    queue: Arc<Mutex<Queue>>,
+    pub view: OnEventView<Panel<LinearLayout>>,
 }
 
 const QUEUE_ID: &str = "queue_list";
 
 impl QueueView {
     pub fn new(queue: Arc<Mutex<Queue>>, event_manager: EventManager) -> QueueView {
-        let mut queuelist = OnEventView::new(ListView::new().with_id(QUEUE_ID));
+        // let queuelist = ListView::new().with_id(QUEUE_ID);
+        let queuelist = SelectView::<String>::new().with_id(QUEUE_ID);
+
+        let layout = LinearLayout::vertical().child(
+            queuelist
+                .full_width()
+                .scrollable()
+                .full_width()
+                .full_height(),
+        );
+        let panel = Panel::new(layout).title("Queue");
+
+        let mut queuelist = OnEventView::new(panel);
 
         {
             let queue = queue.clone();
             // <d> removes the selected track without playing it.
-            queuelist.set_on_pre_event('c', move |_cursive| {
+            queuelist.set_on_event('c', move |_cursive| {
                 queue.lock().unwrap().clear();
             });
         }
@@ -41,13 +53,14 @@ impl QueueView {
             let event_manager = event_manager.clone();
             // <enter> dequeues the selected track
             queuelist.set_on_pre_event(Key::Enter, move |siv| {
-                siv.call_on_id(QUEUE_ID, |queuelist: &mut ListView| {
+                siv.call_on_id(QUEUE_ID, |queuelist: &mut SelectView| {
+                    let selected_id = queuelist.selected_id().unwrap();
                     let track = queue
                         .lock()
                         .unwrap()
-                        .remove(queuelist.focus())
+                        .remove(selected_id)
                         .expect("could not dequeue track");
-                    event_manager.send(Event::SongChange(track));
+                    event_manager.send(Event::Play(track));
                     event_manager.send(Event::QueueUpdate);
                 });
             });
@@ -57,49 +70,44 @@ impl QueueView {
             let queue = queue.clone();
             let event_manager = event_manager.clone();
             // <d> removes the selected track without playing it.
-            queuelist.set_on_pre_event('d', move |siv| {
-                siv.call_on_id(QUEUE_ID, |queuelist: &mut ListView| {
+            queuelist.set_on_event('d', move |siv| {
+                siv.call_on_id(QUEUE_ID, |queuelist: &mut SelectView| {
+                    let selected_id = queuelist.selected_id().unwrap();
                     queue
                         .lock()
                         .unwrap()
-                        .remove(queuelist.focus())
+                        .remove(selected_id)
                         .expect("could not dequeue track");
                     event_manager.send(Event::QueueUpdate);
                 });
             });
         }
 
-        let scrollable = ScrollView::new(queuelist.full_width())
-            .full_width()
-            .full_height();
-        let layout = LinearLayout::vertical().child(scrollable);
-        let panel = Panel::new(layout).title("Queue");
-
-        QueueView {
-            view: Some(panel),
-            queue,
-        }
+        QueueView { view: queuelist }
     }
 
-    pub fn redraw(&self, s: &mut Cursive) {
-        s.call_on_id(QUEUE_ID, |queuelist: &mut ListView| {
+    pub fn redraw(s: &mut Cursive, queue: Arc<Mutex<Queue>>) {
+        s.call_on_id(QUEUE_ID, |queuelist: &mut SelectView| {
+            let selected_id = queuelist.selected_id();
             queuelist.clear();
 
-            let queue = self.queue.lock().unwrap();
+            let queue = queue.lock().unwrap();
             for track in queue.iter() {
-                queuelist.add_child(
-                    "",
-                    TextView::new(format!(
-                        "{} - {}",
-                        track.name,
-                        track
-                            .artists
-                            .iter()
-                            .map(|a| a.name.clone())
-                            .collect::<Vec<String>>()
-                            .join(", ")
-                    )),
+                let label = format!(
+                    "{} - {}",
+                    track.name,
+                    track
+                        .artists
+                        .iter()
+                        .map(|a| a.name.clone())
+                        .collect::<Vec<String>>()
+                        .join(", ")
                 );
+                queuelist.add_item(label, String::new());
+            }
+
+            if let Some(selected_id) = selected_id {
+                queuelist.set_selection(selected_id);
             }
         });
     }
